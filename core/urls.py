ba4714afbb07d4d17134f2588a8bc49cec2f8bb6
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2022-03-30 04:30:24 +0100

    run initial migrations

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ from django.urls import include, path
 
 urlpatterns = (
     [
-        path("admin/", admin.site.urls),
         path("auth/", include("djoser.urls")),
         path("auth/", include("djoser.urls.jwt")),
     ]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+import email
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -7,6 +8,7 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
     username = None
+    email=models.EmailField(_('email address'), unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
